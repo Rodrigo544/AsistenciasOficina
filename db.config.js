76b5d0302ad214409373a17319fa385d8756ce59
Rodrigo544 <r.quiroz2002@hotmail.com>
2021-10-01 19:15:24 -0500

    configuracion inicial de mi proyecto

---
+++ db.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+HOST: "localhost",
+
+USER: "root",
+
+PASSWORD: "1234",
+
+DB: "mysql",
+
+dialect: "mysql",
+
+pool: {
+
+max: 5,
+
+min: 0,
+
+acquire: 30000,
+
+idle: 10000
+
+}
+
+};
